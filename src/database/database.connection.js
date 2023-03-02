51901f2f138fe43960c2a2bc072452ebd43685bb
drebraga <dre.braga@gmail.com>
2023-03-02 11:40:56 -0300

    fix

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -6,9 +6,10 @@ dotenv.config();
 const { Pool } = pkg;
 
 const configDataBase = {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: process.env.DATABASE_URL
 };
+
+if (process.env.MODE === "prod") configDataBase.ssl = true;
 
 const db = new Pool(configDataBase);
 
